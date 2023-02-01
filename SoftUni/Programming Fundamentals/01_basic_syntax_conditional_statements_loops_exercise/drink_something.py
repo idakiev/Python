f5ec3decfa3a09707371a8f654fa74e9d01041c4
Ivo Idakiev <i_idakiev@abv.bg>
2023-02-01 22:56:27 +0200

    Exercise

---
+++ SoftUni/Programming Fundamentals/01_basic_syntax_conditional_statements_loops_exercise/drink_something.py
@@ -0,0 +1,11 @@
+
+persons_age = int(input())
+
+if persons_age <= 14:
+    print("drink toddy")
+elif persons_age <= 18:
+    print("drink coke")
+elif persons_age <= 21:
+    print("drink beer")
+elif persons_age > 21:
+    print("drink whisky")
