af4559e47effb91d5d922e46b2776b6b794f6f20
Ivo Idakiev <i_idakiev@abv.bg>
2023-02-09 22:39:36 +0200

    New exercise

---
+++ SoftUni/Programming Fundamentals/02_data_types_and_variables_exercise/gladiator_expenses.py
@@ -0,0 +1,23 @@
+
+lost_fights = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+
+expenses = 0
+shield_brake_counter = 0
+
+for i in range(1, lost_fights + 1):
+    if i % 2 == 0:
+        expenses += helmet_price
+    if i % 3 == 0:
+        expenses += sword_price
+        if i % 2 == 0:
+            expenses += shield_price
+            shield_brake_counter += 1
+            if shield_brake_counter == 2:
+                expenses += armor_price
+                shield_brake_counter = 0
+
+print(f"Gladiator expenses: {expenses:.2f} aureus")
