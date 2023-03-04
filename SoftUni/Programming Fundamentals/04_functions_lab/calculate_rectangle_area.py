fbb59bc37f09d08a3ebb054f7eebadb9c869bd39
Ivo Idakiev <i_idakiev@abv.bg>
2023-03-04 11:21:21 +0200

    Exercise

---
+++ SoftUni/Programming Fundamentals/04_functions_lab/calculate_rectangle_area.py
@@ -0,0 +1,10 @@
+
+
+def rectangle_area(width: int, height: int):
+    return width * height
+
+
+rectangle_width = int(input())
+rectangle_height = int(input())
+
+print(rectangle_area(rectangle_width, rectangle_height))
