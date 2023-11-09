0371e0cf060f3be1339cef5b9a30938a685727c8
Ivo Idakiev <i_idakiev@abv.bg>
2023-11-09 20:36:27 +0200

    Exercise

---
+++ SoftUni/Programming Advanced - Python/02_tuples_and_sets-lab/summation_pairs_2.py
@@ -0,0 +1,24 @@
+import time
+
+numbers = list(map(int, input(). split()))
+target = int(input())
+
+start = time.time()
+
+targets = set()
+values_map = {}
+
+for value in numbers:
+    if value in targets:
+        targets.remove(value)
+        pair = values_map[value]
+        del values_map[value]
+        print(f"{pair} + {value} = {target}")
+    else:
+        resulting_number = target - value
+        targets.add(resulting_number)
+        values_map[resulting_number] = value
+
+# print("--- %s seconds ---" % (time.time() - start))
+end = time.time()
+print(f"Time range: {end-start}")
